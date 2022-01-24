89ffb82725655885a3c96c3a67857be6e2626fa2
Сергей Усков <q3mydoom@hotmail.com>
2022-01-24 10:12:16 +0500

    Добавьте файлы проекта.

---
+++ Homework 8.6 Task 1/Program.cs
@@ -0,0 +1,47 @@
+﻿/// <summary>
+/// заполнение коллекции
+/// </summary>
+List<int> RandomFillList()
+{
+    List<int> ls = new();
+    Random rnd = new();
+
+    for (int i = 0; i < 100; i++)
+    {
+        ls.Add(rnd.Next(0, 100));
+    }
+
+    return ls;
+}
+
+/// <summary>
+/// вывод коллекции на экран
+/// </summary>
+void PrintList (List<int> ls)
+{
+    for (int i = 0; i < ls.Count; i++)
+    {
+        Console.WriteLine($"{ls[i]}");
+    }
+}
+
+/// <summary>
+/// удаление чисел больше 25 и меньше 50
+/// </summary>
+List<int> RemoveElements(List<int> ls)
+{
+    ls.RemoveAll(item => item > 25 && item < 50);
+    return ls;
+}
+
+List<int> ls = RandomFillList();
+
+PrintList(ls);
+
+Console.WriteLine("удалили числа > 25 и < 50");
+
+RemoveElements(ls);
+
+PrintList(ls);
+
+Console.ReadKey();
